ecc8c109598e52d1767487d2c266cd44586834b3
Ian James <injms@users.noreply.github.com>
2020-11-16 21:29:35 +0000

    🧪 Update pc2dp tests

--- spec/pc2dp.spec.js
+++ spec/pc2dp.spec.js
@@ -1,30 +1,28 @@
-describe('Capitalise protoype extension', function() {
-  beforeAll( function() {
-    Number.prototype.pc2dp = require('../modules/pc2dp')
-  })
+const pc2dp = require('../modules/pc2dp')
 
+describe('Capitalise protoype extension', function() {
   it('turns a number into a percentage', function() {
-    const example = (1).pc2dp()
+    const example = pc2dp(1)
     expect(example).toBe(100)
 
-    const example2 = (0.51).pc2dp()
+    const example2 = pc2dp(0.51)
     expect(example2).toBe(51)
 
-    const example3 = (0.51000).pc2dp()
+    const example3 = pc2dp(0.51000)
     expect(example3).toBe(51)
 
-    const example4 = (0).pc2dp()
+    const example4 = pc2dp(0)
     expect(example4).toBe(0)
   })
 
   it('goes to two decimal places', function() {
-    const example = (0.5625).pc2dp()
+    const example = pc2dp(0.5625)
     expect(example).toBe(56.25)
 
-    const example2 = (0.562525).pc2dp()
+    const example2 = pc2dp(0.562525)
     expect(example2).toBe(56.25)
 
-    const example3 = (0.11555).pc2dp()
+    const example3 = pc2dp(0.11555)
     expect(example3).toBe(11.56)
   })
 })
